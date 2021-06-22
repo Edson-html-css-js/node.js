aacdbd396d26cbf44854574ebe8d69042d8f8faa
Edson-html-css-js <ed_ls@outlook.com>
2021-06-22 14:50:19 -0300

    node.js
    praticando

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'legal'
+console.log(qualquer)
+
+console.log(typeof qualquer)
+
+qualquer=3.1314
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evite nome genéricos e siglas
+let valor =''
+let numero = 1
+let pqp = false //produto quimico perigoso...kkkk
