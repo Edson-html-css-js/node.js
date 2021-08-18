d89ed1c69f040290111bc6350e9443ab2cf600ae
Edson-html-css-js <ed_ls@outlook.com>
2021-08-18 19:24:37 -0300

    node.js

---
+++ node/modulocliente.js
@@ -0,0 +1,10 @@
+const moduloA = require ('./moduloA')
+const moduloB = require ('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.atelogo)
+
+console.log(moduloB.bomdia)
+console.log(moduloB.boanoite())
+console.log(moduloB)
